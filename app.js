47683c06db0d147b393b7019c88d187dcabfc532
Stanley <stanley.wilder.24@gmail.com>
2022-02-24 21:03:38 -0800

    lab 8 and 9 were completed

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ console.log("show me you work!!");
 let userName = "Stanley";
 console.log(userName);
 
-let question = prompt("Do you enjoy pokemon? There's cutes ones here!");
+function greetUser() {
 
-let answer = ('Welcome Trainer!');
 
-if (question == 'yes') {
-   document.write(answer);
-   } else {
-       document.write('YOU SUCK!! POKEMON ARE AWESOME!! Welcome anyways!');
-   }
+    var question = prompt("Do you enjoy pokemon? There's cute ones here!").toLowerCase();
+    console.log(question);
 
+    let answer = ('Welcome Trainer!');
+
+    if (question == 'yes') {
+        document.write(answer);
+
+    } else {
+        alert("wrong answer");
+     greetUser();
+        // document.write('YOU SUCK!! POKEMON ARE AWESOME!! Welcome anyways!');
+    }
+
+}
+function howMany() {
+    let questionTwo = prompt('How many Charizards would you like to see?');
+for (let i = 0; i < questionTwo; i++) {
+    document.write('<img src="https://static.pokemonpets.com/images/monsters-images-300-300/6-Charizard.webp" alt="Charizard">')
+}
+}
